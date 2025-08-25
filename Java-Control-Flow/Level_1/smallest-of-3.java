39d6037cf64aa9c8569bedf320ee97be466cdf04
Krish Dubey <krish.dubey_cs.aiml24@gla.ac.in>
2025-08-25 16:06:11 +0530

    Feat= Added Smallest of 3

---
+++ Java-Control-Flow/Level_1/smallest-of-3.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class SmallestCheck {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Input three numbers
+        System.out.print("Enter first number: ");
+        int number1 = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int number2 = sc.nextInt();
+
+        System.out.print("Enter third number: ");
+        int number3 = sc.nextInt();
+
+        // Check if the first number is the smallest
+        boolean isSmallest = (number1 < number2) && (number1 < number3);
+
+        // Output
+        System.out.println("Is the first number the smallest? " + isSmallest);
+
+        sc.close();
+    }
+}
